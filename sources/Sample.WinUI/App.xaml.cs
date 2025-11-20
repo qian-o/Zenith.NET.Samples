6d15e720581fa91fb48ac53961caffb269971045
qian-o <1324771795@qq.com>
2025-11-20 14:31:39 +0800

    Refactor and enhance Avalonia, WPF, and WinUI apps
    Updated namespaces and bindings for consistency across Avalonia, WPF, and WinUI. Introduced MVVM patterns using `CommunityToolkit.Mvvm` for improved maintainability.
    
    Enhanced the WinUI app with Vulkan-based rendering setup and added a new `MainViewModel` implementation. Updated `App.xaml.cs` to initialize a `GraphicsContext` and activate the `MainView`.
    
    Refactored `MainView.axaml` and `MainView.xaml` to align with the new naming conventions. Added new `MainView.xaml` and `MainView.xaml.cs` for WinUI. Updated the project file to enable preview language features and include necessary package references.

--- sources/Sample.WinUI/App.xaml.cs
+++ sources/Sample.WinUI/App.xaml.cs
@@ -1,17 +1,31 @@
-﻿using Microsoft.UI.Xaml;
+﻿using System.Diagnostics;
+using Microsoft.UI.Xaml;
+using Sample.WinUI.Views;
+using Zenith.NET;
+using Zenith.NET.Views.WinUI;
+using Zenith.NET.Vulkan;
 
 namespace Sample.WinUI;
 
 public partial class App : Application
 {
+    static App()
+    {
+        Context = GraphicsContext.CreateVulkan(true);
+        Context.ValidationMessage += static (sender, args) => Debug.WriteLine($"[{args.Source} - {args.Severity}] {args.Message}");
+
+        Renderer.Initialize(Context, ZenithView.Output);
+    }
+
     public App()
     {
         InitializeComponent();
     }
 
-    public static Window MainWindow { get; } = new();
+    public static GraphicsContext Context { get; }
 
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
+        new Window() { Content = new MainView() }.Activate();
     }
 }
---
+++ sources/Sample.WinUI/Views/MainView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Sample.WinUI.Views;
+
+public sealed partial class MainView : Page
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
