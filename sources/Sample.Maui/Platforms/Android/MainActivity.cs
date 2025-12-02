2c3b279d74ec4fc1f8287b2add4ab3d74b477dca
qian-o <1324771795@qq.com>
2025-12-02 21:29:50 +0800

    Enable edge-to-edge display and enforce landscape mode
    Updated `MainActivity` to support edge-to-edge display by:
    - Adding `Window` configuration for display cutout handling.
    - Using `WindowInsetsControllerCompat` to hide system bars and enable transient bar behavior.
    - Calling `EdgeToEdge.Enable(this)` for additional functionality.
    
    Modified `[Activity]` attribute to enforce landscape orientation with `ScreenOrientation.UserLandscape`.
    
    Added necessary `using` directives for new functionality.

--- sources/Sample.Maui/Platforms/Android/MainActivity.cs
+++ sources/Sample.Maui/Platforms/Android/MainActivity.cs
@@ -1,7 +1,25 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using AndroidX.Activity;
+using AndroidX.Core.View;
 
 namespace Sample.Maui.Platforms.Android;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity;
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ScreenOrientation = ScreenOrientation.UserLandscape, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        Window?.Attributes?.LayoutInDisplayCutoutMode = LayoutInDisplayCutoutMode.ShortEdges;
+
+        WindowInsetsControllerCompat? compat = WindowCompat.GetInsetsController(Window, Window?.DecorView);
+        compat?.SystemBarsBehavior = WindowInsetsControllerCompat.BehaviorShowTransientBarsBySwipe;
+        compat?.Hide(WindowInsetsCompat.Type.SystemBars());
+
+        EdgeToEdge.Enable(this);
+    }
+}
