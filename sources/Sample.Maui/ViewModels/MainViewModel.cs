f67eb22d02cf3bd02130c355b1599afb2d5a9ceb
qian-o <1324771795@qq.com>
2026-02-02 17:05:53 +0800

    Add SharpMetal and log Metal device on Apple platforms
    Added SharpMetal package to solution and project dependencies. Updated MainViewModel to create and log the default Metal device name on iOS and Mac Catalyst using SharpMetal, guarded by platform-specific directives.

--- sources/Sample.Maui/ViewModels/MainViewModel.cs
+++ sources/Sample.Maui/ViewModels/MainViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using SharpMetal.Metal;
 using Zenith.NET.Views;
 
 namespace Sample.Maui.ViewModels;
 
 public partial class MainViewModel : ObservableRecipient
 {
+    public MainViewModel()
+    {
+#if IOS || MACCATALYST
+        var device = MTLDevice.CreateSystemDefaultDevice();
+
+        Console.WriteLine($"Running on device: {device.Name}");
+#endif
+    }
+
     [ObservableProperty]
     public partial string Sample { get; set; } = Renderer.Samples.FirstOrDefault() ?? string.Empty;
 
